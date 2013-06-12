78bad5f91b027d4180ac996e9848d2c534e8fb13
Reimerei <reimerei@github.com>
2013-06-13 00:19:02 +0200

    openshift mongo debugging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
 import play.api.mvc._
+import play.api.{Logger, Play}
+import play.api.Play.current
 
 object Application extends Controller {
 
   def index = Action {
+
+    Logger.debug(Play.configuration.getString("mongodb.uri").getOrElse("Moo"))
+
     Ok(views.html.index("KolibriNet"))
   }
 
