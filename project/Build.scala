4965d769f4756c2594d79667f40ddd44061b086f
Michael Merz <michael.merz@indarium.de>
2013-06-20 15:13:05 +0200

    removed war stuff, moved to war_test branch

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,6 @@
 import sbt._
 import Keys._
 import play.Project._
-import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
@@ -21,10 +20,8 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies)
-    .settings(Play2WarPlugin.play2WarSettings: _*)
     .settings(
     // Add your own project settings here
-    Play2WarKeys.servletVersion := "3.0"
   )
 
 }
