7d564c45bea77dc32046ee53d53ea26cb499cf48
Reimerei <reimerei@github.com>
2013-07-21 15:08:58 +0200

    Can now upload two or more assets with one commit

--- app/controllers/AssetController.scala
+++ app/controllers/AssetController.scala
@@ -10,11 +10,10 @@ import reactivemongo.api.gridfs.GridFS
 import helper.IdHelper
 import play.api.Logger
 import models.{Asset, Message}
-import reactivemongo.core.commands.LastError
 import scala.Some
-import reactivemongo.bson.BSONString
 import reactivemongo.api.gridfs.Implicits.DefaultReadFileReader
 import java.util.Date
+import reactivemongo.core.commands.LastError
 
 /**
  * User: Björn Reimer
@@ -30,62 +29,68 @@ object AssetController extends ExtendedController {
   def uploadAsset(token: String, messageId: String) =
     authenticateGET[MultipartFormData[Future[ReadFile[BSONValue]]]](token, bodyParser = gridFSBodyParser(gridFS)) {
       (username, request) => {
-        val futureFile = request.body.files.head.ref
-        val assetId = IdHelper.generateAssetId()
-
+        val futureFiles = request.body.files
         Async {
           // check if the message exist
           Message.find(messageId).map {
-              case None => BadRequest(resKO("Message not found"))
-              case Some(message) => {
-                // add message and asset ids to file
-                val futureResult = for {
-                  file <- futureFile
-                  assetResult <- {
-                    val query = BSONDocument("_id" -> file.id)
-                    val set = BSONDocument("$set" -> BSONDocument(
-                      "messageId" -> messageId,
-                      "assetId" -> assetId,
-                      "user" -> message.from,
-                      "created" -> BSONDateTime((new java.util.Date).getTime)))
+            case None => BadRequest(resKO("Message not found"))
+            case Some(message) => {
+              val futureAssetIds: Seq[Future[(String, LastError)]] = futureFiles.map {
+                futureFile => {
+                  for {
+                    assetId <- Future(IdHelper.generateAssetId())
+                    file <- futureFile.ref
+                    // add messageId and assetId to file in gridfs
+                    assetResult <- {
+                      val query = BSONDocument("_id" -> file.id)
+                      val set = BSONDocument("$set" -> BSONDocument(
+                        "messageId" -> messageId,
+                        "assetId" -> assetId,
+                        "user" -> message.from,
+                        "created" -> BSONDateTime((new java.util.Date).getTime)))
 
-                    implicit val fileReader = DefaultReadFileReader
-                    gridFS.files.update(query, set)
-                  }
-                  messageResult <- {
-                    // create asset object
-                    val asset = new Asset(
-                      assetId,
-                      String.valueOf(file.chunkSize),
-                      file.filename,
-                      file.contentType.getOrElse("unknown"),
-                      new Date)
-
-                    // add asset to message
-                    val query = Json.obj("conversationId" -> message.conversationId,
-                      "messages.messageId" -> message.messageId)
-                    val set = Json.obj("$push" -> Json.obj("messages.$.assets" -> asset))
-                    conversationCollection.update(query, set)
-                  }
-                } yield (messageResult, assetResult)
-
-                Async {
-                  futureResult.map {
-                    case (messageResult: LastError, assetResult) => {
-                      if (messageResult.inError) {
-                        Logger.error("Error updating message: " + messageResult.stringify)
-                        InternalServerError(resKO("Error updating message: " + messageResult.stringify))
-                      } else {
-                        Ok(resOK(Json.obj("assetId" -> assetId)))
-                      }
+                      implicit val fileReader = DefaultReadFileReader
+                      gridFS.files.update(query, set)
                     }
-                  }
+                    messageResult <- {
+                      // create asset object
+                      val asset = new Asset(
+                        assetId,
+                        String.valueOf(file.chunkSize),
+                        file.filename,
+                        file.contentType.getOrElse("unknown"),
+                        new Date)
+
+                      // add asset to message
+                      val query = Json.obj("conversationId" -> message.conversationId,
+                        "messages.messageId" -> message.messageId)
+                      val set = Json.obj("$push" -> Json.obj("messages.$.assets" -> asset))
+                      conversationCollection.update(query, set)
+                    }
+                  } yield (assetId, messageResult)
+                }
+              }
+              Async {
+                Future.sequence(futureAssetIds).map {
+                  results =>
+
+                    val errors = results.filter(_._2.inError)
+
+                    if (errors.length > 0) {
+                      Logger.error("Error updating message: " + errors)
+                      InternalServerError(resKO(JsArray(errors.map { case (id,
+                      error) => Json.obj(id -> error.stringify) })))
+                    } else {
+                      Ok(resOK(Json.obj("assetIds" -> results.map { case (id, error) => id })))
+                    }
                 }
               }
             }
           }
         }
+      }
     }
+
 
   def getAsset(token: String, assetId: String) = authenticateGET(token) {
     (username, request) =>
@@ -93,10 +98,10 @@ object AssetController extends ExtendedController {
         val file = gridFS.find(BSONDocument("assetId" -> new BSONString(assetId)))
         // Frontend always wants inline
         serve(gridFS, file, CONTENT_DISPOSITION_INLINE)
-//        request.getQueryString("inline") match {
-//          case Some("true") => serve(gridFS, file, CONTENT_DISPOSITION_INLINE)
-//          case _ => serve(gridFS, file)
-//        }
+        //        request.getQueryString("inline") match {
+        //          case Some("true") => serve(gridFS, file, CONTENT_DISPOSITION_INLINE)
+        //          case _ => serve(gridFS, file)
+        //        }
       }
   }
 }
