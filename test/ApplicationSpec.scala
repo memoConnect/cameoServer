f7cebc1271f734441831436c26c0d8a610acfae0
Michael Merz <michael.merz@indarium.de>
2014-01-22 08:58:16 +0100

    fixed simple tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -14,19 +14,13 @@ class ApplicationSpec extends Specification {
   
   "Application" should {
     
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
-      }
-    }
-    
     "render the index page" in {
       running(FakeApplication()) {
         val home = route(FakeRequest(GET, "/")).get
         
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
+        contentAsString(home) must contain ("login")
       }
     }
   }
