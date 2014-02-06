4c07258c47885a1f7d8658a2ee1340208ae2ac65
Reimerei <git@bjrm.de>
2014-02-06 18:33:16 +0100

    implemented pipeline definition

--- app/controllers/MessageController.scala
+++ app/controllers/MessageController.scala
@@ -66,38 +66,57 @@ object MessageController extends ExtendedController {
     implicit val format: Format[FilterRules] = createMongoFormat(Json.reads[FilterRules], Json.writes[FilterRules])
   }
 
-  def filter() = AuthAction.async(parse.tolerantJson) {
+  def filter(offset: Int = 0, varLimit: Int = 0) = AuthAction.async(parse.tolerantJson) {
     request =>
 
+      // set default limit
+      val limit = if (varLimit < 1) 150 else varLimit
+
       request.body.validate[FilterRules].map {
-        fr => {
-          // create pipeline
-          val from = fr.fromIdentities.getOrElse(Seq) ++ Contact.
-        }
+        fr =>
+          {
+            def collectIdentities(ids: Option[Seq[String]], groups: Option[Seq[String]]): Seq[MongoId] = {
+              ids.getOrElse(Seq()).map(s => new MongoId(s)) ++
+                groups.getOrElse(Seq()).flatMap {
+                  group =>
+                    request.identity.getGroup(group).map { _.identityId }
+                }
+            }
 
+            // create pipeline
+            val conversations = request.identity.conversations
+            val from: Seq[MongoId] = collectIdentities(fr.fromIdentities, fr.fromGroups)
+            val to: Seq[MongoId] = collectIdentities(fr.toIdentities, fr.toGroups)
 
+            def createMatch(ids: Seq[MongoId], matchFunction: (MongoId => JsObject)): Match = {
+              if (ids.size < 1) {
+                Match(toBson(Json.obj()).get)
+              }
+              else {
+                val matchJson = Json.obj("$or" -> ids.map(id => Json.obj("_id" -> id)))
+                Match(toBson(matchJson).get)
+              }
+            }
 
-      }
+            val pipeline: Seq[PipelineOperator] = Seq(
+              createMatch(conversations, id => Json.obj("_id" -> id)),
+              Unwind("messages"),
+              Project(("messages", BSONInteger(1)), ("_id", BSONInteger(-1))),
+              createMatch(from, id => Json.obj("messages.fromIdentityId" -> id)),
+              createMatch(to, id => Json.obj("messages.messageStatus.identityId" -> id)),
+              Sort(Seq(Ascending("messages.created"))),
+              Skip(offset),
+              Limit(limit)
+            )
 
-      // first get all conversations of this user
-      val matchConversations = Json.obj("$or" -> request.identity.conversations.map(c => Json.obj("_id" -> Json.toJson(c))))
+            val aggregationCommand = Aggregate(Conversation.col.name, pipeline)
 
-      val pipeline: Seq[PipelineOperator] = Seq(
-        Match(toBson(matchConversations).get),
-        Unwind("messages"),
-        Project(("messages", BSONInteger(1)), ("_id", BSONInteger(-1))),
-        Sort(Seq(Ascending("messages.created"))) //,
-      //        Limit(10)
-      )
-
-      val aggregationCommand = Aggregate(Conversation.col.name, pipeline)
-
-      mongoDB.command(aggregationCommand).map {
-        res =>
-
-          val messages: Seq[JsValue] = res.force.toList.map(Json.toJson(_))
-
-          Ok(messages.toString())
-      }
+            mongoDB.command(aggregationCommand).map {
+              res =>
+                val messages: Seq[JsValue] = res.force.toList.map(Json.toJson(_))
+                Ok(messages.toString())
+            }
+          }
+      }.recoverTotal(error => Future.successful(BadRequest(resKO(JsError.toFlatJson(error)))))
   }
 }
