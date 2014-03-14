58f954bec9ee4d73c5e3af04e97d80ad481e5cbc
play <reimerei@github.com>
2014-03-14 13:58:07 +0000

    updated deploy scripts - now they are pretty

--- update.sh
+++ update.sh
@@ -2,11 +2,28 @@
 
 set -e
 
+case "$1" in
+   "server")
+	updateServer=true
+      ;;
+   "client")
+	updateClient=true
+      ;;
+   "both")
+	updateClient=true
+	updateServer=true
+      ;;
+   *)
+      echo -e "\e[33m[cameo - Update type required: [server|client|both] ]\033[0m"
+      exit 1
+      ;;
+esac
+
 fileName="UPDATING"
 # check if we are alreade updating
 if [ -e ${fileName} ]; then
     echo -e "\e[33m[cameo - another update is running, update scheduled]\033[0m"
-    echo "+1" > ${fileName}
+    echo $1 > ${fileName}
     exit 0
 else
     touch ${fileName}
@@ -26,31 +43,35 @@ else
 fi
 
 # update server
-echo -e "\e[33m[cameo - updating server]\033[0m"
-git pull
-
-#update client
-if [ -d ../cameoJSClient ]; then
-    echo -e "\e[33m[cameo - updating client]\033[0m"
-    cd ../cameoJSClient
-    git pull
-    ./compile.sh
-    echo -e "\e[33m[cameo - copying client dist to public]\033[0m"
-    cp -r dist/* ../cameoServer/public/
-    cd -
-else
-    echo -e "\e[33m[cameo - directory cameoJSClient not found]\033[0m"
-    exit 1
+if [ "$updateServer" = true ]; then
+	echo -e "\e[33m[cameo - updating server]\033[0m"
+	git pull
 fi
 
+#update client
+if [ "$updateClient" = true ]; then
+	if [ -d ../cameoJSClient ]; then
+	    echo -e "\e[33m[cameo - updating client]\033[0m"
+	    cd ../cameoJSClient
+	    git pull
+	    ./compile.sh
+	    echo -e "\e[33m[cameo - copying client dist to public]\033[0m"
+	    cp -r dist/* ../cameoServer/public/
+	    cd - &> /dev/null
+	else
+	    echo -e "\e[33m[cameo - directory cameoJSClient not found]\033[0m"
+	    exit 1
+	fi
+fi
 ./compile.sh
 ./start.sh ${mode}
 
 # check if another update is sheduled
 if [ -s ${fileName} ]; then
-    echo -e "\e[33m[cameo - found scheduled update, starting it now]\033[0m"
+	nextMode=$(cat ${fileName})
+    echo -e "\e[33m[cameo - found scheduled update, starting it now. Mode: ${nextMode}]\033[0m"
     rm ${fileName}
-    ./update.sh
+    ./update.sh ${nextMode}
 else
     rm ${fileName}
 fi
