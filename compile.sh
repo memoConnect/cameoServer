9f37aa3943d17e9db835486b2836a8a8209cb664
Reimerei <git@bjrm.de>
2014-03-26 17:44:54 +0100

    made compile version more rubust

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # write version to file
-echo application.version=$1 > ./conf/version.conf
+if [ -z "$1" ]; then
+    version=$1
+else
+    version="none"
+fi
 
-echo -e "\e[33m[cameo - Compiling version: ${version}]\033[0m"
+echo application.version=${version} > ./conf/version.conf
+
+echo -e "\e[33m[cameo - Compiling version: $1 ]\033[0m"
 ./sbt clean compile stage
