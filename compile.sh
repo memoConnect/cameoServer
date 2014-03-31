969c4510a0ec7dbebaea453aff340d7940d1b02c
Reimerei <git@bjrm.de>
2014-03-31 18:15:32 +0200

    added quick compile option

--- compile.sh
+++ compile.sh
@@ -10,4 +10,9 @@ fi
 echo application.version=${version} > ./conf/version.conf
 
 echo -e "\e[33m[ CameoServer - Compiling version: ${version} ]\033[0m"
-./sbt clean compile stage
+
+if [ "$2" == "quick" ]; then
+    ./sbt compile stage
+else
+    ./sbt clean compile stage
+fi
