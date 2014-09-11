744eb22888caa30c095d756220c7c7066632525f
reimerei <github@bjrm.de>
2014-09-11 12:51:16 +0200

    added version check to getBrowserInfo

--- app/helper/Utils.scala
+++ app/helper/Utils.scala
@@ -2,9 +2,10 @@ package helper
 
 import java.math.BigInteger
 
-import play.api.Play
+import play.api.{Logger, Play}
 import play.api.Play.current
 
+import scala.annotation.tailrec
 import scala.sys.process.stringSeqToProcess
 
 /**
@@ -48,6 +49,30 @@ object Utils {
 
   def md5Long(s: String): Long = {
     md5BigInteger(s).longValue()
+  }
+
+  case class InvalidVersionException(msg: String) extends RuntimeException
+
+  // returns true if b is a higher than or equal to a.
+  def compareVersions(versionA: String, versionB: String): Boolean = {
+    val aInt = versionA.split('.').toList.map(safeStringToInt(_).getOrElse(throw new InvalidVersionException(versionA)))
+    val bInt = versionB.split('.').toList.map(safeStringToInt(_).getOrElse(throw new InvalidVersionException(versionB)))
+
+    if(aInt.isEmpty) throw new InvalidVersionException(versionA)
+    if(bInt.isEmpty) throw new InvalidVersionException(versionB)
+
+    @tailrec
+    def check(listA: List[Int], listB: List[Int]): Boolean = {
+      (listA, listB) match {
+        case (Nil, Nil) => true
+        case (Nil, _)   => true
+        case (_, Nil)   => false
+        case (headA :: restA, headB :: restB) if headA <= headB =>
+          check(restA, restB)
+        case _ => false
+      }
+    }
+    check(aInt, bInt)
   }
 
 }
